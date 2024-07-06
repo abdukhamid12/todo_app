45c124dbfc3c2f0f98aa20cbf19e13676881d7ec
Smoke_Front_Back_Developing <116000706+abdukhamid12@users.noreply.github.com>
2024-07-06 17:05:29 +0500

    17:05

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task.views import home
+from task.views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('modal_create/', modal_create, name='modal_create')
 ]
--- task/views.py
+++ task/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'task/index.html')
+
+def modal_create(request):
+    return render(request, 'task/modal_create.html')
